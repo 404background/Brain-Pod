de2f5ce7357f2be541c0931038f220134c388648
404background <404background@gmail.com>
2023-04-02 17:05:51 +0900

    add : SoftwaSerial & switch-case

--- Pod_sub/Pod_sub.ino
+++ Pod_sub/Pod_sub.ino
@@ -1,19 +1,44 @@
 //A0:バッテリー電圧確認用（https://wiki.seeedstudio.com/XIAO_ESP32C3_Getting_Started/）
 
 #include "BNO055.h"
+#include "SoftwareSerial.h"
+
+SoftwareSerial mySerial;
+
+char cmd;     //for switch-case
+uint32_t Vbatt = 0;
+float Vbattf = 0;
 
 void setup() {
   Serial.begin(115200);
+  mySerial.begin(115200);
   pinMode(A0, INPUT);         // ADC
   BNO055_setup();
 }
 
 void loop() {
-  uint32_t Vbatt = 0;
-  for(int i = 0; i < 16; i++) {
-    Vbatt = Vbatt + analogReadMilliVolts(A0); // ADC with correction   
+  if (Serial.available()) {
+    cmd = Serial.read();
   }
-  float Vbattf = 2 * Vbatt / 16 / 1000.0;     // attenuation ratio 1/2, mV --> V
-  Serial.println(Vbattf, 3);
-  delay(1000);
+  if (mySerial.available()) {
+    cmd = mySerial.read();
+  }
+
+  switch (cmd) {
+    case '1':
+      BNO055();
+      break;
+      
+/*Check battery voltage****************************************************************************/
+    case '0':     
+      for (int i = 0; i < 16; i++) {
+        Vbatt = Vbatt + analogReadMilliVolts(A0); // ADC with correction
+      }
+      Vbattf = 2 * Vbatt / 16 / 1000.0;     // attenuation ratio 1/2, mV --> V
+      Serial.println(Vbattf, 3);
+      mySerial.println(Vbattf, 3);
+      break;
+    default:
+      delay(100);
+  }
 }
